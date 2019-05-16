d78af7452284aeed292c87ad328154ff1183c181
tukida <tuankiet.hcmc@gmail.com>
2019-05-16 17:10:23 +0700

    Fix bug send data via data channel

--- client.js
+++ client.js
@@ -58,11 +58,10 @@ conn.onerror = function (err) {
  * @param {*} key 
  */
 function cryptoraphy(data, key) {
-    console.log(data)
+    data = new Uint8Array(data)
     var dataLength = data.byteLength
     let result = new Uint8Array(dataLength)
     for (var i = 0; i < dataLength; i++) {
-        console.log(data[i])
         result[i] = data[i] ^ key[i % key.byteLength]
     }
     return result
@@ -147,6 +146,13 @@ var callBtn = document.querySelector('#callBtn');
 var hangUpBtn = document.querySelector('#hangUpBtn');
 callPage.style.display = "none";
 
+var msgInput = document.querySelector('#msgInput');
+var sendMsgBtn = document.querySelector('#sendMsgBtn');
+var chatArea = document.querySelector('#chatarea');
+
+var yourConn;
+var dataChannel;
+
 // Login when the user clicks the button 
 loginBtn.addEventListener("click", function (event) {
     name = usernameInput.value;
@@ -171,6 +177,119 @@ function handleLogin(success) {
         //********************** 
         //Starting a peer connection 
         //********************** 
+
+        //using Google public stun server 
+        var configuration = {
+            "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
+        };
+
+        yourConn = new webkitRTCPeerConnection(configuration);
+
+        // Setup ice handling 
+        yourConn.onicecandidate = function (event) {
+            if (event.candidate) {
+                send({
+                    type: "candidate",
+                    candidate: event.candidate
+                });
+            }
+        };
+
+        //creating data channel 
+        dataChannel = yourConn.createDataChannel("channel", { reliable: true });
+        yourConn.ondatachannel  = function (event) {
+            dataChannel = event.channel
+        }
+        dataChannel.onerror = function (error) {
+            console.log("Ooops...error:", error);
+        };
+
+        //when we receive a message from the other peer, display it on the screen 
+        dataChannel.onmessage = function (event) {
+            chatArea.innerHTML += connectedUser + ": " + event.data + "<br />";
+        };
+
+        dataChannel.onclose = function () {
+            console.log("data channel is closed");
+        };
     }
 
 };
+
+//initiating a call
+callBtn.addEventListener("click", function () {
+    var callToUsername = callToUsernameInput.value;
+
+    if (callToUsername.length > 0) {
+
+        connectedUser = callToUsername;
+
+        // create an offer 
+        yourConn.createOffer(function (offer) {
+
+            send({
+                type: "offer",
+                offer: offer
+            });
+
+            yourConn.setLocalDescription(offer);
+
+        }, function (error) {
+            alert("Error when creating an offer");
+        });
+    }
+});
+
+//when somebody sends us an offer 
+function handleOffer(offer, name) {
+    connectedUser = name;
+    yourConn.setRemoteDescription(new RTCSessionDescription(offer));
+
+    //create an answer to an offer 
+    yourConn.createAnswer(function (answer) {
+        yourConn.setLocalDescription(answer);
+
+        send({
+            type: "answer",
+            answer: answer
+        });
+
+    }, function (error) {
+        alert("Error when creating an answer");
+    });
+};
+
+//when we got an answer from a remote user 
+function handleAnswer(answer) {
+    yourConn.setRemoteDescription(new RTCSessionDescription(answer));
+};
+
+//when we got an ice candidate from a remote user 
+function handleCandidate(candidate) {
+    yourConn.addIceCandidate(new RTCIceCandidate(candidate));
+};
+
+//hang up 
+hangUpBtn.addEventListener("click", function () {
+    send({
+        type: "leave"
+    });
+
+    handleLeave();
+});
+
+function handleLeave() {
+    connectedUser = null;
+    yourConn.close();
+    yourConn.onicecandidate = null;
+};
+
+//when user clicks the "send message" button 
+sendMsgBtn.addEventListener("click", function (event) {
+    var val = msgInput.value;
+    chatArea.innerHTML += name + ": " + val + "<br />";
+
+    //sending a message to a connected peer 
+    dataChannel.send(val);
+    msgInput.value = "";
+});
